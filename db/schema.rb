0ea2159cf15a7dfa4a47c7a00d619b68675f7777
thedannysoto <87dsoto@gmail.com>
2020-04-10 14:33:32 -0500

    modifies User table to include uid and image for Facebook authorization

--- db/schema.rb
+++ db/schema.rb
@@ -30,9 +30,10 @@ ActiveRecord::Schema.define(version: 2020_04_09_190352) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "name"
     t.string "email"
+    t.integer "uid"
+    t.string "image"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
