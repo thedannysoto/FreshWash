8c4fa6c643bc9914653d005c125801a406e2bedb
thedannysoto <87dsoto@gmail.com>
2020-04-10 22:25:04 -0500

    modifies index page and adds a create user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
     def new 
-
+        @user = User.new
     end
 
     def create 
-        
+        User.create(user_params)
+    end
+
+
+    private
+ 
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
