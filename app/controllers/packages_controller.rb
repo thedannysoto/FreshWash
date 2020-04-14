3b034ef519685f9bde4d29cd792257eb018ea53e
thedannysoto <87dsoto@gmail.com>
2020-04-14 15:33:44 -0500

    packages/new page creates packages and links to car and wash

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -5,6 +5,7 @@ class PackagesController < ApplicationController
     end
 
     def new
+        @packages = Package.all
         @package = Package.new
         @car = Car.find(params[:car_id])
         @user = current_user
@@ -12,12 +13,22 @@ class PackagesController < ApplicationController
     end
 
     def create 
-
+        package = Package.find(params[:package][:package_id])
+        car = Car.find(params[:car_id])
+        wash = Wash.new
+        wash.package = package
+        wash.car = car 
+        wash.save
+        redirect_to user_path(current_user)
     end
 
     private 
 
     def current_user
         User.find(session[:user_id])
+    end
+
+    def package_params
+        params.require(:package).permit(:package_id)
     end
 end
